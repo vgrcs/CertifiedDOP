9bc456196e730a0bd5ee779d61b271bc395f521b
Vitor Rodrigues <vitor@di.unito.it>
2017-04-07 17:29:42 +0200

    update

---
+++ test/Main.hs
@@ -0,0 +1,25 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  Profiler
+-- Copyright   :
+-- License     :  AllRightsReserved
+--
+-- Maintainer  :  Vitor Rodrigues
+-- Stability   :
+-- Portability :
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+module Main ( main
+
+) where
+
+import Criterion.Main
+import System.Process
+
+main =  do
+        let cmd = "./delta-cert apply -v -r -d ecall_delta-1.0-1.i686.drpm"
+            prog = readCreateProcessWithExitCode (shell cmd) ""
+        defaultMain [ bgroup "checker"  [ bench "10" $ whnfIO prog ] ]
